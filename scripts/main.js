3484f564efa4b25eeed95706175e9b516eacb3ea
elizaveta.shvedchikova <eli3avetta.shvedchikova@gmail.com>
2023-09-30 18:46:45 +0300

    save

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 const LS_SAVE_POST = 'SAVED_POST';
 const LS_FONT_FAMILY = 'FONT_FAMILY';
+const LS_FONT_SIZE = 'FONT_SIZE';
 
 const postRef = document.getElementById('post-ref');
 const linkDownloadBtn = document.getElementById('link-download');
 const generateTextBtn = document.getElementById('generate-text');
 const saveBtn = document.getElementById('save');
-const fontInputs = document.querySelectorAll('[name="font"]');
+const fontFamilyBtns = document.querySelectorAll('[name="font"]');
+const fontSizeInput = document.querySelector('[name="font-size"]');
 
 linkDownloadBtn.onclick = () => {
   html2canvas(postRef)
@@ -41,10 +43,10 @@ saveBtn.onclick = () => {
 const fontFamilyDefault = localStorage.getItem(LS_FONT_FAMILY);
 if (fontFamilyDefault) {
   setFontFamily(fontFamilyDefault);
-  Array.from(fontInputs).find(input => input.value === fontFamilyDefault).checked = true
+  Array.from(fontFamilyBtns).find(input => input.value === fontFamilyDefault).checked = true
 }
 
-fontInputs.forEach(input => {
+fontFamilyBtns.forEach(input => {
   input.oninput = () => {
     setFontFamily(input.value);
     localStorage.setItem(LS_FONT_FAMILY, input.value)
@@ -53,4 +55,19 @@ fontInputs.forEach(input => {
 
 function setFontFamily(font) {
   postRef.style.fontFamily = `var(--font-family-${ font })`;
+}
+
+const fontSizeDefault = localStorage.getItem(LS_FONT_SIZE);
+if (fontSizeDefault) {
+  fontSizeInput.value = fontSizeDefault;
+  setFontSize(fontSizeDefault);
+}
+
+fontSizeInput.oninput = () => {
+  setFontSize(fontSizeInput.value);
+}
+
+function setFontSize(size) {
+  postRef.style.fontSize = `${ size }px`;
+  localStorage.setItem(LS_FONT_SIZE, size);
 }
